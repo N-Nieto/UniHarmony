f4154177fbc53c8c51ffbdf0ce52821c7fa91bcc
N-Nieto <nnieto@sinc.unl.edu.ar>
2026-01-31 13:08:57 +0100

    feat: update gitignore to ignore local copy stored in data +  update utils and uv.lock

---
+++ src/uniharmony/_utils.py
@@ -0,0 +1,26 @@
+"""General utilities."""
+
+__all__ = [
+    "filter_site_by_size",
+]
+
+
+import numpy as np
+
+
+def filter_site_by_size(sites, min_size, max_size=np.inf, sites_ignore=None):
+    idx = np.zeros(len(sites))
+    if sites_ignore is None:
+        sites_ignore = np.array([])
+    for su in sites_ignore:
+        idx_su = sites == su
+        idx = np.logical_or(idx, idx_su)
+    sites_check = np.setdiff1d(np.unique(sites), sites_ignore)
+    for su in sites_check:
+        idx_su = sites == su
+        n = np.sum(idx_su)
+        if n >= min_size and n < max_size:
+            idx = np.logical_or(idx, idx_su)
+        else:
+            print(f"excluding site: {su} #sub {n}")
+    return idx
