e4f07d608fab64d29f3512e1644ebddfaa519d61
Synchon Mandal <synchon@protonmail.com>
2026-03-09 17:24:07 +0100

    chore(interpolation): update typing and docstrings

--- src/uniharmony/interpolation/_utils.py
+++ src/uniharmony/interpolation/_utils.py
@@ -9,6 +9,7 @@ from imblearn.over_sampling import (
     KMeansSMOTE,
     RandomOverSampler,
 )
+from numpy.typing import npt
 from sklearn.utils import check_random_state
 
 
@@ -21,8 +22,37 @@ __all__ = [
 
 def create_interpolator(
     name: str, random_state: int | np.random.RandomState = 23, **kwargs
+) -> (
+    type[SMOTE]
+    | type[BorderlineSMOTE]
+    | type[SVMSMOTE]
+    | type[ADASYN]
+    | type[KMeansSMOTE]
+    | type[RandomOverSampler]
 ):
-    """Create an imblearn interpolator based on a string name."""
+    """Create an imblearn interpolator based on a string name.
+
+    Parameters
+    ----------
+    name : str
+        Name of interpolator.
+    random_state : int or RandomState instance, optional (default 23)
+        The seed of the pseudo random number generator or RandomState for
+        reproducibility.
+    **kwargs : dict
+        Extra keyword arguments for the interpolator.
+
+    Returns
+    -------
+    object
+        Initialised interpolator instance.
+
+    Raises
+    ------
+    ValueError
+        If ``name`` is invalid.
+
+    """
     random_state = check_random_state(random_state)
     mapping = {
         "smote": SMOTE,
@@ -42,8 +72,23 @@ def create_interpolator(
     )
 
 
-def sites_sanity_checks(x, sites):
-    """Sanity checks for site array."""
+def sites_sanity_checks(x: npt.NDArray, sites: npt.NDArray) -> None:
+    """Sanity checks for site array.
+
+    Parameters
+    ----------
+    x : array
+        Features.
+    sites : array
+        Sites.
+
+    Raises
+    ------
+    ValueError
+        If ``x`` and ``sites`` have different length or
+        if single site is provided.
+
+    """
     if x.shape[0] != sites.shape[0]:
         raise ValueError("X and sites must have same length")
 
@@ -51,8 +96,22 @@ def sites_sanity_checks(x, sites):
         raise ValueError("At least two sites required")
 
 
-def class_representation_checks(y, sites):
-    """Check that each site has at least two classes."""
+def class_representation_checks(y: npt.NDArray, sites: npt.NDArray) -> None:
+    """Check that each site has at least two classes.
+
+    Parameters
+    ----------
+    y : array
+        Targets.
+    sites : array
+        Sites.
+
+    Raises
+    ------
+    ValueError
+        If ``sites`` have single class.
+
+    """
     for site in np.unique(sites):
         if len(np.unique(y[sites == site])) < 2:
             raise ValueError(
