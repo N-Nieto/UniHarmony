8e54c5921b0ce38f7b9e7780b675fa4bf4c6493e
Synchon Mandal <synchon@protonmail.com>
2026-04-09 12:08:41 +0200

    chore: expose plot subpackage at package level

--- src/uniharmony/__init__.pyi
+++ src/uniharmony/__init__.pyi
@@ -5,11 +5,12 @@ __all__ = [
     "load_MAREoS",
     "make_multisite_classification",
     "normative",
+    "plot",
     "prettyharmonize",
     "verbosity",
     "verbosity_context",
 ]
 
-from . import combat, dl, interpolation, normative, prettyharmonize
+from . import combat, dl, interpolation, normative, plot, prettyharmonize
 from ._verbose import verbosity, verbosity_context
 from .datasets import load_MAREoS, make_multisite_classification
