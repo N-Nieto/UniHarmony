f8cffef8bcc3d03a80639f66e9f8cf78edcce604
N-Nieto <nnieto@sinc.unl.edu.ar>
2026-04-20 21:50:10 +0200

    chore: clean multisite characterization, add MAD metric. Rename main function for better consistency

--- src/uniharmony/datasets/__init__.pyi
+++ src/uniharmony/datasets/__init__.pyi
@@ -1,5 +1,5 @@
 __all__ = [
-    "get_site_data_statistics",
+    "get_multisite_data_statistics",
     "load_MAREoS",
     "make_multisite_classification",
     "print_statistics_summary",
@@ -7,4 +7,4 @@ __all__ = [
 
 from ._load_mareos import load_MAREoS
 from ._make_multisite_classification import make_multisite_classification
-from ._multisite_data_characterization import get_site_data_statistics, print_statistics_summary
+from ._multisite_data_characterization import get_multisite_data_statistics, print_statistics_summary
