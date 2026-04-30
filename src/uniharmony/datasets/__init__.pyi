aac8cdc26b9fb8107f23777feb7328582a2c0158
N-Nieto <nnieto@sinc.unl.edu.ar>
2026-05-01 00:51:56 +0200

    add helper functions in init

--- src/uniharmony/datasets/__init__.pyi
+++ src/uniharmony/datasets/__init__.pyi
@@ -1,4 +1,7 @@
 __all__ = [
+    "_get_onharmony_information_form_idps",
+    "_list_available_files",
+    "_list_available_possibilities",
     "get_multisite_data_statistics",
     "load_MAREoS",
     "load_onharmony",
@@ -7,6 +10,11 @@ __all__ = [
 ]
 
 from ._load_mareos import load_MAREoS
-from ._load_onharmony import load_onharmony
+from ._load_onharmony import (
+    _get_onharmony_information_form_idps,
+    _list_available_files,
+    _list_available_possibilities,
+    load_onharmony,
+)
 from ._make_multisite_classification import make_multisite_classification
 from ._multisite_data_characterization import get_multisite_data_statistics, print_statistics_summary
