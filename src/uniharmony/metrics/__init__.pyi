05db3bf8c577534135552713493691dc18591545
N-Nieto <nnieto@sinc.unl.edu.ar>
2026-05-04 19:36:57 +0200

    add functions to init

--- src/uniharmony/metrics/__init__.pyi
+++ src/uniharmony/metrics/__init__.pyi
@@ -1,5 +1,30 @@
 __all__ = [
+    "METRICS_REQUIRING_Y_PRED",
+    "METRICS_REQUIRING_Y_SCORE",
+    "_input_checks",
+    "_input_checks_multi",
+    "_is_binary_or_multiclass",
+    "_is_probability_like",
+    "_metric_needs_y_pred",
+    "_metric_needs_y_score",
+    "_validate_metric_kwargs",
+    "_validate_prediction_type",
     "report_metric_by_site",
+    "report_metric_by_site",
+    "report_metrics_by_site",
 ]
 
-from ._report_metric_by_site import report_metric_by_site
+from ._report_metric_by_site import (
+    METRICS_REQUIRING_Y_PRED,
+    METRICS_REQUIRING_Y_SCORE,
+    _input_checks,
+    _input_checks_multi,
+    _is_binary_or_multiclass,
+    _is_probability_like,
+    _metric_needs_y_pred,
+    _metric_needs_y_score,
+    _validate_metric_kwargs,
+    _validate_prediction_type,
+    report_metric_by_site,
+    report_metrics_by_site,
+)
