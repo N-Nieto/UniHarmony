126e803111e3c42be8b4fb994d549f53daec9c6d
synchon <synchon@users.noreply.github.com>
2026-05-04 16:16:08 +0000

    deploy: eea44c34292a7401996a28019c4a2634a6b18b33

---
+++ html/_sources/api/utils/verbosity.md
@@ -0,0 +1,5 @@
+# verbosity
+
+```{eval-rst}
+.. autofunction:: uniharmony.verbosity
+```
--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.3.dev69',
+    VERSION: '0.0.3.dev74',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
