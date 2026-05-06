89b2310df01cddf598249af50395c72d031c4536
N-Nieto <nnieto@sinc.unl.edu.ar>
2026-05-06 04:43:45 +0200

    test: parametrize test

--- tests/datasets/test_load_onharmony.py
+++ tests/datasets/test_load_onharmony.py
@@ -1,24 +1,28 @@
 """Test module for load_onharmony function."""
 
+import pytest
+
 from uniharmony.datasets import load_onharmony
 
 
-def test_load_onharmony_success() -> None:
+@pytest.mark.parametrize(
+    "subjects, sessions, modalities, suffixes, extensions, copy, force_download",
+    [
+        # Binary classification tests
+        ("16981", "NOT4GEP001", "anat", "T2w", ".json", False, True),
+        ("all", "NOT4GEP001", "anat", "T1w", ".json", False, True),
+        ("16981", "all", "anat", "T1w", ".json", False, False),
+        ("16981", "all", "all", "T1w", ".json", False, False),
+    ],
+)
+def test_load_onharmony_success(subjects, sessions, modalities, suffixes, extensions, copy, force_download) -> None:
     """Test basic functionality."""
     load_onharmony(
-        subjects="16981",
-        sessions="NOT4GEP001",
-        modalities="anat",
-        data_types="T2w",
-        extensions=".json",
-        copy=False,
-        force_download=True,
-    )
-    load_onharmony(
-        subjects="16981",
-        sessions="NOT4GEP001",
-        modalities="anat",
-        data_types="T1w",
-        extensions=".json",
-        copy=False,
+        subjects=subjects,
+        sessions=sessions,
+        modalities=modalities,
+        suffixes=suffixes,
+        extensions=extensions,
+        copy=copy,
+        force_download=force_download,
     )
