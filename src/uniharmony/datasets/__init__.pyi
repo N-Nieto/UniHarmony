7c3ca838363b4c45f59eeec8299934d1e326f13f
Synchon Mandal <synchon@protonmail.com>
2026-05-06 17:28:08 +0200

    update: clean up imports for datasets

--- src/uniharmony/datasets/__init__.pyi
+++ src/uniharmony/datasets/__init__.pyi
@@ -1,32 +1,20 @@
 __all__ = [
-    "_list_available_possibilities",
-    "clean_tmp_folder",
+    "clean_tmp",
     "download_bids_dataset",
-    "get_candidate_files",
-    "get_files",
     "get_multisite_data_statistics",
-    "initialize_dl_dataset",
     "list_available_files",
     "load_MAREoS",
     "load_ONharmony",
     "make_multisite_classification",
     "print_statistics_summary",
-    "validate_arguments",
 ]
 
 from ._datalad_integration import (
-    clean_tmp_folder,
+    clean_tmp,
     download_bids_dataset,
-    get_candidate_files,
-    get_files,
-    initialize_dl_dataset,
     list_available_files,
-    validate_arguments,
 )
 from ._load_mareos import load_MAREoS
-from ._load_onharmony import (
-    _list_available_possibilities,
-    load_ONharmony,
-)
+from ._load_onharmony import load_ONharmony
 from ._make_multisite_classification import make_multisite_classification
 from ._multisite_data_characterization import get_multisite_data_statistics, print_statistics_summary
