d3830a209f3109aced3e7cd1e2266bf9383f24dd
Synchon Mandal <synchon@protonmail.com>
2026-05-06 17:27:15 +0200

    docs: replace ipynb with py

---
+++ examples/02-multisite-data/04-run_load_on_harmony.py
@@ -0,0 +1,35 @@
+"""
+Load ON-Harmony dataset
+=======================
+"""
+
+# %%
+# Imports
+# -------
+
+from uniharmony import verbosity
+from uniharmony.datasets import clean_tmp_folder, load_ONharmony
+
+
+verbosity("debug")
+
+
+# %%
+# Load dataset
+# ------------
+
+load_ONharmony(
+    subjects="16981",
+    sessions="NOT4GEP001",
+    modalities="anat",
+    suffixes="T1w",
+    extensions=".json",
+)
+
+
+# %%
+# Clean up
+# --------
+
+# Later, clean up the temporary cache
+clean_tmp_folder("datalad_cache")
